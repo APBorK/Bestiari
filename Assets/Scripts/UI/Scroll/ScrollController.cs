6d308077b505f1ddd0077281d7178bebb0a646be
APBorK <1659732@gmail.com>
2023-02-01 12:28:15 +0300

    Fix code

--- Assets/Scripts/UI/Scroll/ScrollController.cs
+++ Assets/Scripts/UI/Scroll/ScrollController.cs
@@ -1,18 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.UIElements;
-using Button = UnityEngine.UI.Button;
 
 public class ScrollController : MonoBehaviour
 {
     private ScrollRect _rectScroll;
-    private RectTransform _originalContent, _lastContent;
+    private RectTransform _lastContent;
     void Start()
     {
         _rectScroll = GetComponent<ScrollRect>();
-        _originalContent = _rectScroll.content;
     }
 
     public void OpenPanel(GameObject content)
@@ -22,25 +17,8 @@ public class ScrollController : MonoBehaviour
         _rectScroll.content = content.GetComponent<RectTransform>();
     }
 
-    public void Back()
-    {
-        
-    }
-    
     void ColsedPanel(GameObject content)
     {
         content.SetActive(false);
-    }
-    
-    
-    protected void VercticalScroll(RectTransform rectTransformConten,float maxScroll)
-    {
-        //_rectTransformContent.anchoredPosition = new Vector2(rectTransformConten.anchoredPosition.x,
-          // Mathf.Clamp(rectTransformConten.anchoredPosition.y,_minScroll,maxScroll) );
-    }
-    
-    void SearchMaxScroll(Button maxScroll,Button offset)
-    {
-        //_maxScroll = Mathf.Abs(maxScroll.transform.localPosition.y - offset.transform.localPosition.y);
     }
 }
--- Assets/Scripts/UI/Writer.cs
+++ Assets/Scripts/UI/Writer.cs
@@ -3,20 +3,21 @@ using UnityEngine.UI;
 
 public class Writer : MonoBehaviour
 {
-    [SerializeField] private GameObject _panel;
+    [SerializeField] private GameObject panel;
+    private const int SizeText = 200;
     private string _text;
     private Text _list;
 
     private void Start()
     {
-        _list = _panel.GetComponentInChildren<Text>();
+        _list = panel.GetComponentInChildren<Text>();
     }
 
-    public void LoadText(GameObject _nameText)
+    public void LoadText(GameObject nameText)
     {
-        _text = Resources.Load<TextAsset>("Text/" + _nameText.name).text;
+        _text = Resources.Load<TextAsset>("Text/" + nameText.name).text;
         _list.text = _text;
-        _panel.GetComponent<RectTransform>().sizeDelta =
-            new Vector2(_panel.GetComponent<RectTransform>().sizeDelta.x, _list.preferredHeight + 200);
+        panel.GetComponent<RectTransform>().sizeDelta =
+            new Vector2(panel.GetComponent<RectTransform>().sizeDelta.x, _list.preferredHeight + SizeText);
     }
 }
