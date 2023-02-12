c0fdbf91ad0f8808dd2ec9ddd9ce9c02da65ad0d
APBorK <1659732@gmail.com>
2023-02-12 20:00:32 +0300

    SystemLoadExemple

--- Assets/Scripts/UI/Writer.cs
+++ Assets/Scripts/UI/Writer.cs
@@ -5,6 +5,7 @@ public class Writer : MonoBehaviour
 {
     [SerializeField] private GameObject _panel;
     [SerializeField] private GameObject _panelExemple;
+    private GameObject _textExemple;
     private const int SizeText = 300;
     private string _text;
     private Text _list;
@@ -13,6 +14,7 @@ public class Writer : MonoBehaviour
     private void Start()
     {
         _list = _panel.GetComponentInChildren<Text>();
+        _textExemple = _panelExemple.GetComponentInChildren<Text>().gameObject;
     }
 
     public void LoadText(GameObject nameText)
@@ -20,9 +22,13 @@ public class Writer : MonoBehaviour
         _nameText = nameText.name;
         _text = Resources.Load<TextAsset>("Text/" + _nameText).text;
         _list.text = _text;
-        EventSystem.SendOpenText(nameText.name);
         _panel.GetComponent<RectTransform>().sizeDelta =
             new Vector2(_panel.GetComponent<RectTransform>().sizeDelta.x, _list.preferredHeight + SizeText);
+        GameObject example =  Resources.Load<GameObject>("Prefabs/Text/" + _nameText);
+        Destroy(_textExemple);
+        _textExemple = Instantiate(example,_panelExemple.transform);
+        _panelExemple.GetComponent<RectTransform>().sizeDelta =
+            new Vector2(_panel.GetComponent<RectTransform>().sizeDelta.x, _textExemple.GetComponent<Text>().preferredHeight + SizeText);
     }
 
     //public void OpenExempleText()
